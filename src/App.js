d0cdfd732f7da5dd2a27e413d9828d42210b4f07
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2022-09-08 13:48:23 -0500

    added cart context and cart dropdown

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from "./components/routes/home/home.component";
 import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authentication/authentication.component';
+import Shop from './components/routes/shop/shop.component'
 
 const App = () => {
 
@@ -10,6 +11,7 @@ return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
 				<Route path='auth' element={<Authentication />} />
 			</Route>
 		</Routes>
